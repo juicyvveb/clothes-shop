0d931345fd8b8a6c1a2f90d9e12bb8cef2293c38
luisfendi@protonmail.com <luisfendi@protonmail.com>
2022-07-13 16:40:21 +0300

    ошибка потерия соединения не очень работает

--- src/main.js
+++ src/main.js
@@ -2,27 +2,27 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import {router} from './router';
 import {store} from './store';
-import Network from './components/NoNetwork.vue';
+// import Network from './components/NoNetwork.vue';
 
-let promise = fetch('https://www.google.com');
-promise
-.then(
-    () => {
+// let promise = fetch('https://www.google.com');
+// promise
+// .then(
+//     () => {
+//         const app = createApp(App);
+//         app.use(store);
+//         app.use(router);
+//         app.mount('#app');
+//     },
+//     () =>{ 
+//         // store.commit('stateError', e.message);
+//         const app = createApp(Network);
+//         app.mount('#app');
+//     })
+//     // .then(() => {
         const app = createApp(App);
         app.use(store);
         app.use(router);
         app.mount('#app');
-    },
-    () =>{ 
-        // store.commit('stateError', e.message);
-        const app = createApp(Network);
-        app.mount('#app');
-    })
-    // .then(() => {
-    //     const app = createApp(App);
-    //     app.use(store);
-    //     app.use(router);
-    //     app.mount('#app');
     // })
 
 
